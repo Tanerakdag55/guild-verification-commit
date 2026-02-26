189fdf760c5ffb9f18185f343ee944a8226f6efc
Tanerakdag55 <118531237+Tanerakdag55@users.noreply.github.com>
2026-02-26 21:14:17 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-21T22:13:34.505Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-02-26T18:14:16.712Z
+Commit: Patch API endpoint error handling
